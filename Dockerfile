7ec53fd3c79355ab315e5a0d2ea7d7c3b3112104
Regner Blok-Andersen <shadowdf@gmail.com>
2016-03-01 10:25:59 +0000

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+
+
+FROM debian:latest
+MAINTAINER Regner Blok-Andersen <shadowdf@gmail.com>
+
+RUN apt-get update -qq
+RUN apt-get upgrade -y -qq
+RUN apt-get install -y -qq python-dev python-pip
+RUN pip install -qU pip
+
+ENV GOOGLE_APPLICATION_CREDENTIALS "path-to-credentials.json"
+ENV GCLOUD_DATASET_ID "your gce project"
+
+ADD en_motd.py /en_motd/
+ADD requirements.txt /en_motd/
+
+WORKDIR /en_rss/
+
+RUN pip install -r requirements.txt
+
+CMD python /en_motd/en_motd.py
---
+++ en_motd.py
@@ -0,0 +1,82 @@
+
+
+import os
+import json
+import logging
+import requests
+
+from time import sleep
+from gcloud import datastore, pubsub
+
+logging.basicConfig()
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# App Settings
+SLEEP_TIME = int(os.environ.get('SLEEP_TIME', 30))
+EN_TOPIC_SETTINGS = os.environ.get('EN_TOPIC_SETTINGS', 'http://en-topic-settings:80/external')
+
+# Datastore Settings
+DS_CLIENT = datastore.Client()
+SERVICE_KIND = 'EN-MOTD'
+
+# PubSub Settings
+PS_CLIENT = pubsub.Client()
+PS_TOPIC = PS_CLIENT.topic(os.environ.get('NOTIFICATION_TOPIC', 'send_notification'))
+
+if not PS_TOPIC.exists():
+    PS_TOPIC.create()
+
+
+def send_notification(title, subtitle, topic):
+    topics = json.dumps([topic])
+    
+    PS_TOPIC.publish(
+        '',
+        title=title,
+        subtitle=subtitle,
+        service='en-motd',
+        topics=topics,
+    )
+
+
+def get_services():
+    response = requests.get(EN_TOPIC_SETTINGS)
+    response.raise_for_status()
+
+    return response.json()['motd']['topics']
+
+
+def update_latest_entry(motd, latest_id, new_latest_id):
+    if latest_id is None:
+        latest_id = datastore.Entity(DS_CLIENT.key(SERVICE_KIND, 'latest-id', 'MOTD', motd))
+        latest_id['id'] = new_latest_id
+        DS_CLIENT.put(latest_id)
+
+    if latest_id['id'] != new_latest_id:
+        latest_id['id'] = new_latest_id
+        DS_CLIENT.put(latest_id)
+    
+
+while True:
+    services = get_services()
+    
+    for motd in services:
+        logger.info('Checking "{}" for updated message.'.format(motd['name']))
+
+        response = requests.get(motd['url'])
+        response.raise_for_status()
+        
+        motd_data = response.json()
+
+        latest_id = DS_CLIENT.get(DS_CLIENT.key(SERVICE_KIND, 'latest-id', 'MOTD', motd['name']))
+        
+        if latest_id is not None and latest_id['id'] == motd_data['id']:
+            continue
+
+        logger.info('New entry found for "{}"! Entry title: {}'.format(motd['name'], motd_data['motd']['title']))
+        
+        send_notification(motd['name'], motd_data['motd']['title'], motd['topic'])
+        update_latest_entry(motd['name'], latest_id, motd_data['id'])
+
+    sleep(SLEEP_TIME)
